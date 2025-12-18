59c6d25cc0abede8ae11da7c795bf588fafbe74d
Rashna Ferdous <rashnaferdous65@gmail.com>
2025-12-19 00:22:21 +0600

    Add bootstrap-4 pagination blade view

--- resources/views/vendor/pagination/bootstrap-4.blade.php
+++ resources/views/vendor/pagination/bootstrap-4.blade.php
@@ -1,46 +1,61 @@
-@if ($paginator->hasPages())
-    <nav>
-        <ul class="pagination">
-            {{-- Previous Page Link --}}
-            @if ($paginator->onFirstPage())
-                <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
-                    <span class="page-link" aria-hidden="true">&lsaquo;</span>
-                </li>
+@if($paginator->hasPages())
+<nav>
+    <ul class="pagination">
+
+        {{-- Previous --}}
+        <li class="page-item {{ $paginator->onFirstPage() ? 'disabled' : '' }}">
+            @if($paginator->onFirstPage())
+                <span class="page-link">&lsaquo;</span>
             @else
-                <li class="page-item">
-                    <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</a>
+                <a class="page-link"
+                   href="{{ $paginator->previousPageUrl() }}"
+                   rel="prev"
+                   aria-label="@lang('pagination.previous')">
+                    &lsaquo;
+                </a>
+            @endif
+        </li>
+
+        {{-- Pages --}}
+        @foreach($elements as $item)
+
+            {{-- Dots --}}
+            @if(is_string($item))
+                <li class="page-item disabled">
+                    <span class="page-link">{{ $item }}</span>
                 </li>
             @endif
 
-            {{-- Pagination Elements --}}
-            @foreach ($elements as $element)
-                {{-- "Three Dots" Separator --}}
-                @if (is_string($element))
-                    <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
-                @endif
-
-                {{-- Array Of Links --}}
-                @if (is_array($element))
-                    @foreach ($element as $page => $url)
-                        @if ($page == $paginator->currentPage())
-                            <li class="page-item active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
+            {{-- Page Numbers --}}
+            @if(is_array($item))
+                @foreach($item as $number => $link)
+                    <li class="page-item {{ $number === $paginator->currentPage() ? 'active' : '' }}">
+                        @if($number === $paginator->currentPage())
+                            <span class="page-link">{{ $number }}</span>
                         @else
-                            <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
+                            <a class="page-link" href="{{ $link }}">{{ $number }}</a>
                         @endif
-                    @endforeach
-                @endif
-            @endforeach
-
-            {{-- Next Page Link --}}
-            @if ($paginator->hasMorePages())
-                <li class="page-item">
-                    <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</a>
-                </li>
-            @else
-                <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
-                    <span class="page-link" aria-hidden="true">&rsaquo;</span>
-                </li>
+                    </li>
+                @endforeach
             @endif
-        </ul>
-    </nav>
+
+        @endforeach
+
+        {{-- Next --}}
+        <li class="page-item {{ $paginator->hasMorePages() ? '' : 'disabled' }}">
+            @if($paginator->hasMorePages())
+                <a class="page-link"
+                   href="{{ $paginator->nextPageUrl() }}"
+                   rel="next"
+                   aria-label="@lang('pagination.next')">
+                    &rsaquo;
+                </a>
+            @else
+                <span class="page-link">&rsaquo;</span>
+            @endif
+        </li>
+
+    </ul>
+</nav>
 @endif
+
